26f50fb0edf53edef5278fd30eeb1c6879357805
Like Zhu <zlike.free@gmail.com>
2021-06-08 11:45:44 -0700

    Update package names

--- packages/overreact/webpack.config.js
+++ packages/overreact/webpack.config.js
@@ -5,5 +5,24 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.bundle.js',
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
     },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    externals: [
+        {
+            react: 'react',
+        },
+    ],
 };
